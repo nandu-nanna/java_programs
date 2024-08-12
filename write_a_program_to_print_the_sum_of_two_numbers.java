b5a7a9a006f73fa0b284783fd9ae515914146112
nandu-nanna <nandinibolla999@gmail.com>
2024-08-12 15:47:17 +0530

    Create write_a_program_to_print_the_sum_of_two_numbers.java

---
+++ write_a_program_to_print_the_sum_of_two_numbers.java
@@ -0,0 +1,7 @@
+class AddNumbers{
+    public static void main(String args[]){
+        int num1=10,num2=20,sum;
+       sum=num1+num2;
+        System.out.println("sum of"+num1+","+num2+"is:"+sum);
+    }
+}
