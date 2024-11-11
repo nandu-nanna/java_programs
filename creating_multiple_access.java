8b07db00803bcf3e3c8f0f30c94089894a1d946a
nandu-nanna <nandinibolla999@gmail.com>
2024-11-11 15:46:22 +0530

    Create creating_multiple_access.java

---
+++ creating_multiple_access.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    int x=5;
+        public static void main(String[] args)
+        {
+        Main myobj1=new Main();
+        Main myobj2=new Main();
+        System.out.println(myobj1.x);
+        System.out.println(myobj2.x);
+    }
+}
